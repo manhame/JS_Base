5f4e199c60ef37498846c358ffb5adb500f0dd65
manhame <respectbiz@gmail.com>
2024-01-25 19:19:47 +0100

    exercice2 essais 4 carrés

--- exercice2/js/base2.js
+++ exercice2/js/base2.js
@@ -2,3 +2,5 @@ const carre1 = document.querySelector(".carre1");
 const carre2 = document.querySelector(".carre2");
 const carre3 = document.querySelector(".carre1");
 const carre4 = document.querySelector(".carre1");
+
+const dialog = document.querySelector("#dialog");
