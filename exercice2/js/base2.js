01eee7b2415fa1359766b4aeed6900fb58cca195
manhame <respectbiz@gmail.com>
2024-01-25 19:01:33 +0100

    essais exercices 1 et 2

---
+++ exercice2/js/base2.js
@@ -0,0 +1,4 @@
+const carre1 = document.querySelector(".carre1");
+const carre2 = document.querySelector(".carre2");
+const carre3 = document.querySelector(".carre1");
+const carre4 = document.querySelector(".carre1");
