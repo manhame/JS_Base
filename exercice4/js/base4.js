1fab568cdac5976feeb1b02fd04f17f871a4d58f
manhame <respectbiz@gmail.com>
2024-01-30 16:37:52 +0100

    fin : intégration "index" dans fonction "forEach" et var "indexIconActive null" pour traiter plus de 2 options (booléen)

--- exercice4/js/base4.js
+++ exercice4/js/base4.js
@@ -1,35 +1,39 @@
-var fb=document.querySelector(".fb");
-var x=document.querySelector(".x");
-var insta=document.querySelector(".insta");
-var icons=document.querySelectorAll("icon");
-var elements=document.querySelectorAll("row");
-let clicked=false;
 
-// Itère sur chaque élément de la liste "icons" en utilisant la méthode forEach
-icons.forEach((icon) => {
-    // Récupère le style calculé (y compris les styles définis par des feuilles de style) de chaque carré
-    
-    const styleIcon = getComputedStyle(icon);
+const icons = document.querySelectorAll(".icon");
+const texts = document.querySelectorAll(".text");
+const row = document.querySelector(".row");//ne récupère que l'élément "row"
+let indexIconActive = null; // l'index de l'icône active (par défaut null <=> aucun/neutre/gris)
 
-    icon.addEventListener("click", function() {
-    /* Lorsqu'un icon est cliqué, efface le style de la classe "icon"
-    et ajoute le style de la classe "iconcliqued" si icon "pas cliqué"*/
-        icons.forEach((i) => {
-            if(!clicked) {
-            
-                i.classList.remove("icon");
-                i.classList.add("iconclicked");
-                clicked = true;
-            }
+console.log("CHECKPOINT 1 - icons = ", icons);
+
+// Itère sur chaque élément de la liste "icons" (classe "icon") en utilisant la méthode forEach
+icons.forEach((icon, indexIcon) => {
+
+    console.log("CHECKPOINT 2 - icon = ", icon);
         
-    /*si "icon cliqué" il efface le style de la classe iconclicked pour
-        remettre celui de la classe icon*/
-                else {
-                    i.classList.remove("iconclicked");
-                    i.classList.add("icon");  
-                    clicked = false;  
-                }
-        });
+    icon.addEventListener("click", function() {
+
+        console.log("CHECKPOINT 3 - getComputedStyle(icon).backgroundColor = ", getComputedStyle(icon).backgroundColor);
+
+        /* Lorsqu'un icon est cliqué, efface le style de la classe "icon"
+        et ajoute le style de la classe "iconCliqued" et inversément + valide le clic*/
+        // si l'utilisateur clique sur une icône non active (=> on veut une couleur)
+        // if(indexIconActive == null || indexIcon != indexIconActive) {
+        if(indexIcon !== indexIconActive) {
+            row.style.backgroundColor = getComputedStyle(icon).backgroundColor;
+            texts[indexIcon].classList.add("text-displayed");
+            icon.classList.add("iconClicked");
+            indexIconActive = indexIcon;
+        }
+        
+        /*si "icon déjà cliqué" il efface le style de la classe iconclicked pour
+        remettre celui de la classe icon + valide le double clic*/
+        // sinon il a cliqué sur l'icône active (=> on veut griser)
+        else {
+            icon.classList.remove("iconClicked");
+            texts[indexIcon].classList.remove("text-displayed");
+            row.style.backgroundColor = "grey";  
+            indexIconActive = null;
+        }
     })
 });
-
